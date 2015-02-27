8b855857ef307f46d48c422bb360b01e247637d7
Ron-e <mail@r0n.nl>
2015-02-27 14:22:34 +0100

    :rage1:

--- Sentora theme installer/theme_installer.sh
+++ Sentora theme installer/theme_installer.sh
@@ -51,13 +51,13 @@ if [[ "$THEME_GITHUB" = "1" ]]; then
 	rm -f $THEME_NAME.zip
 	
 	if [[ "$THEME_UPDATE" = "1" ]]; then
-		rm -rf $THEME_NAME
+		rm -rf /etc/sentora/panel/etc/styles/$THEME_NAME
 	fi
 	
 	mv -u -f $THEME_NAME-master $THEME_NAME
 else
 	if [[ "$THEME_UPDATE" = "1" ]]; then
-		rm -rf $THEME_NAME
+		rm -rf /etc/sentora/panel/etc/styles/$THEME_NAME
 	fi
 	
 	unzip -q $THEME_NAME.zip
